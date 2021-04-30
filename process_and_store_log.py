d0f9121aa6c294655a6639f8ac0d2a0a01d7e509
Le Hai <49013652+lehai2909@users.noreply.github.com>
2021-04-30 17:00:20 +0700

    add code for transforming stage

--- process_and_store_log.py
+++ process_and_store_log.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("log/log-20210430.csv")
+
+df['parsed_time'] = pd.to_datetime(df.timestamp)
+df_new = df[['parsed_time','insertId','jsonPayload.message']]
+df_new.set_index('parsed_time')
